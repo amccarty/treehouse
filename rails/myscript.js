b469bf4864fa12a1aece1b0894b534b08c7eb3fd
Alan McCarty <alan@givegab.com>
2013-09-03 15:32:36 -0400

    initial commit

---
+++ rails/myscript.js
@@ -0,0 +1,15 @@
+//Do a couple of console.logs
+
+console.log("Hello from myscript.js");
+
+console.log("Hello again!"); //This isn't needed
+
+console.log("whatever");
+
+var name = prompt("What is your name?")
+
+alert("Hello " + name);
+
+name = "nick";
+
+console.log("The user's name is " + name);
