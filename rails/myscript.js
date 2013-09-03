5dc3b82ee50455121807cf402c47a19eba040bce
Alan McCarty <alan@givegab.com>
2013-09-03 17:08:41 -0400

    initial commit

--- rails/myscript.js
+++ rails/myscript.js
@@ -4,12 +4,26 @@ console.log("Hello from myscript.js");
 
 console.log("Hello again!"); //This isn't needed
 
+
 console.log("whatever");
 
-var name = prompt("What is your name?")
+/* var name = prompt("What is your name?")
 
 alert("Hello " + name);
 
 name = "nick";
 
 console.log("The user's name is " + name);
+*/
+
+var name = prompt("What is your name?")
+
+console.log("Before");
+
+if (name) {
+	console.log("If block");
+} else {
+	console.log("Else block");
+}
+
+console.log("After");
